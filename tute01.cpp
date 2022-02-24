8accf6b372d5a4aa33fff384b3453c7be1d035d3
sandeepani <99406704+it21264702@users.noreply.github.com>
2022-02-24 02:56:29 +0000

    group 4.2

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,14 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
-int main() 
+#include <iostream>
+using namespace std;
+int main()
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+    float cm,inches;
+    cout<< "enter a length in cm:";
+    cin>> cm;
+    inches= cm/2.54;
+    cout<<"length in inches"<<inches <<endl;
     return 0;
 }  
